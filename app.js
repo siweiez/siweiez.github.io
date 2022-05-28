b115015013438cd885ea1178882f749a1bf9b1a5
siweiez <102887631+siweiez@users.noreply.github.com>
2022-05-28 01:28:24 -0700

    .

--- app.js
+++ app.js
@@ -18,24 +18,16 @@ function PageTransition() {
   allSections.addEventListener('click', (e) => {
     const id = e.target.dataset.id;
     if (id) {
-      //   secBtns.forEach((btn) => {
-      //     btn.classList.remove('active-btn');
-      //   });
-      //   e.target.classList.add('active-btn');
-
       sections.forEach((sect) => {
         sect.classList.remove('active-section');
-        // sect.classList.remove('active');
       });
 
       const element = document.getElementById(id);
       element.classList.add('active-section');
-      // element.classList.add('active');
     }
   });
 
   const theme = document.querySelector('.theme-btn');
-
   theme.addEventListener('click', () => {
     let body = document.body;
     body.classList.toggle('dark-mode');
